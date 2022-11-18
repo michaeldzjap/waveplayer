de97f54dfc0e8ad9d190fed5fc904f2bdf15bf93
Michael Dzjaparidze <michael.dzjaparidze@gmail.com>
2022-11-18 08:32:25 +0100

    Update dependencies

---
+++ src/Bar.ts
@@ -0,0 +1,20 @@
+/**
+ * Bar.ts
+ *
+ * © Michaël Dzjaparidze 2022
+ * https://github.com/michaeldzjap
+ *
+ * A HTML5 based audio player with a waveform view
+ *
+ * This work is licensed under the MIT License (MIT)
+ */
+
+/**
+ * @class
+ * @classdesc Bar class.
+ */
+class Bar {
+    //
+}
+
+export default Bar;
